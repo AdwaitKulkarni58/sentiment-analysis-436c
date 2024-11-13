049b082ab24a94f5c5cef8f4bdcc3107ce0108e5
domlo <domlo@students.cs.ubc.ca>
2024-11-12 16:46:48 -0800

    indent

--- src/components/Visualization.jsx
+++ src/components/Visualization.jsx
@@ -1,11 +1,11 @@
 import { useState, useEffect } from "react";
 
 function Visualization() {
-    return (
-        <>
-            <h1>Here be graphs</h1>
-        </>
-    )
+  return (
+    <>
+      <h1>Here be graphs</h1>
+    </>
+  )
 }
 
 export default Visualization;

c5b2bd3993faabc6e8542628e9ef205495d2be84
domlo <domlo@students.cs.ubc.ca>
2024-11-12 16:33:43 -0800

    goes to viz when submit

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 import Tab from "react-bootstrap/Tab";
 import Tabs from "react-bootstrap/Tabs";
 import CoursesReviewForm from "./components/CoursesReviewForm";
+import Visualization from "./components/Visualization";
 import "./App.css";
 import DataVisualizationDashboard from "./components/DataVisualizationDashboard";
 
 function App() {
+  const [activeTab, setActiveTab] = useState("home")
+
   return (
     <div className="main-container">
       <h1 className="main-title">Sentiment Analysis Rate UBC Courses</h1>
-      <Tabs defaultActiveKey="home" className="tabs">
+      <Tabs activeKey={activeTab} onSelect={(tab) => setActiveTab(tab)} className="tabs">
         <Tab eventKey="home" title="Home" className="tab-content">
-          <CoursesReviewForm />
+          <CoursesReviewForm onSubmitChangeTab={() => setActiveTab("data-viz")} />
         </Tab>
         <Tab
           eventKey="data-viz"
---
+++ src/components/Visualization.jsx
@@ -0,0 +1,11 @@
+import { useState, useEffect } from "react";
+
+function Visualization() {
+    return (
+        <>
+            <h1>Here be graphs</h1>
+        </>
+    )
+}
+
+export default Visualization;

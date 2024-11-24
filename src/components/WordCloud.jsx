2a5ec713aa0e08157d5366d9ee495fbffe9f3f2b
adwait58 <adwait58@students.cs.ubc.ca>
2024-11-23 16:05:28 -0800

    add sentiment count and pie chart

--- src/components/WordCloud.jsx
+++ src/components/WordCloud.jsx
@@ -1,9 +1,10 @@
 import React, { useEffect, useRef } from "react";
 import * as d3 from "d3";
-import cloud from 'd3-cloud';
+import cloud from "d3-cloud";
 
 function WordCloudComponent({ words }) {
-  
+  console.log(words);
+
   const svgRef = useRef();
 
   useEffect(() => {
@@ -13,31 +14,51 @@ function WordCloudComponent({ words }) {
     const layout = cloud()
       .size([500, 300]) // Size of the word cloud
       .words(
-        words.map(word => ({ text: word.text, size: word.value }))
+        words.map(
+          (word) => (
+            console.log(word),
+            {
+              text: word.text,
+              size: word.value,
+            }
+          )
+        )
       )
       .padding(10) // Space between words
-      .fontSize(d => d.size) // Map font size to the word's frequency
+      .fontSize((d) => d.size) // Map font size to the word's frequency
       .rotate(0) // No rotation for readability
       .on("end", draw); // Draw the word cloud once layout is complete
 
     layout.start();
 
     function draw(wordsToDraw) {
-      const svg = d3.select(svgRef.current)
+      const svg = d3
+        .select(svgRef.current)
         .attr("width", layout.size()[0])
         .attr("height", layout.size()[1]);
 
-      svg.append("g")
-        .attr("transform", `translate(${layout.size()[0] / 2},${layout.size()[1] / 2})`)
+      svg
+        .append("g")
+        .attr(
+          "transform",
+          `translate(${layout.size()[0] / 2},${layout.size()[1] / 2})`
+        )
         .selectAll("text")
         .data(wordsToDraw)
-        .enter().append("text")
+        .enter()
+        .append("text")
         .style("font-family", "Impact")
-        .style("fill", () => d3.schemeCategory10[Math.floor(Math.random() * 10)])
-        .style("font-size", d => `${d.size}px`)
+        .style(
+          "fill",
+          () => d3.schemeCategory10[Math.floor(Math.random() * 10)]
+        )
+        .style("font-size", (d) => `${d.size}px`)
         .attr("text-anchor", "middle")
-        .attr("transform", d => `translate(${d.x},${d.y}) rotate(${d.rotate})`)
-        .text(d => d.text);
+        .attr(
+          "transform",
+          (d) => `translate(${d.x},${d.y}) rotate(${d.rotate})`
+        )
+        .text((d) => d.text);
     }
   }, [words]);
 
@@ -45,7 +66,7 @@ function WordCloudComponent({ words }) {
     <>
       <svg ref={svgRef}></svg>
     </>
-  )
+  );
 }
 
 export default WordCloudComponent;

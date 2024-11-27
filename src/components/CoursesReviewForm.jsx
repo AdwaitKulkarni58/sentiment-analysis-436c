747525d68d6c31d3643f4d2ad8b7dae65bc01012
adwait58 <adwait58@students.cs.ubc.ca>
2024-11-27 13:02:42 -0800

    remove courses from dropdown

--- src/components/CoursesReviewForm.jsx
+++ src/components/CoursesReviewForm.jsx
@@ -117,36 +117,36 @@ function CoursesReviewForm({ onSubmitChangeTab }) {
             onChange={handleCourseChange}
             className="select-course"
           >
-            <option value="CPSC100">CPSC 100</option>
+            {/* <option value="CPSC100">CPSC 100</option>
             <option value="CPSC103">CPSC 103</option>
-            <option value="CPSC107">CPSC 107</option>
+            <option value="CPSC107">CPSC 107</option> */}
             <option value="CPSC110">CPSC 110</option>
             <option value="CPSC121">CPSC 121</option>
-            <option value="CPSC203">CPSC 203</option>
+            {/* <option value="CPSC203">CPSC 203</option> */}
             <option value="CPSC210">CPSC 210</option>
             <option value="CPSC213">CPSC 213</option>
             <option value="CPSC221">CPSC 221</option>
-            <option value="CPSC259">CPSC 259</option>
+            {/* <option value="CPSC259">CPSC 259</option>
             <option value="CPSC302">CPSC 302</option>
             <option value="CPSC303">CPSC 303</option>
-            <option value="CPSC304">CPSC 304</option>
+            <option value="CPSC304">CPSC 304</option> */}
             <option value="CPSC310">CPSC 310</option>
-            <option value="CPSC311">CPSC 311</option>
+            {/* <option value="CPSC311">CPSC 311</option>
             <option value="CPSC312">CPSC 312</option>
             <option value="CPSC313">CPSC 313</option>
             <option value="CPSC314">CPSC 314</option>
             <option value="CPSC317">CPSC 317</option>
-            <option value="CPSC319">CPSC 319</option>
+            <option value="CPSC319">CPSC 319</option> */}
             <option value="CPSC320">CPSC 320</option>
-            <option value="CPSC322">CPSC 322</option>
+            {/* <option value="CPSC322">CPSC 322</option>
             <option value="CPSC330">CPSC 330</option>
             <option value="CPSC340">CPSC 340</option>
             <option value="CPSC344">CPSC 344</option>
             <option value="CPSC368">CPSC 368</option>
             <option value="CPSC404">CPSC 404</option>
-            <option value="CPSC406">CPSC 406</option>
+            <option value="CPSC406">CPSC 406</option> */}
             <option value="CPSC410">CPSC 410</option>
-            <option value="CPSC416">CPSC 416</option>
+            {/* <option value="CPSC416">CPSC 416</option>
             <option value="CPSC418">CPSC 418</option>
             <option value="CPSC420">CPSC 420</option>
             <option value="CPSC421">CPSC 421</option>
@@ -154,9 +154,9 @@ function CoursesReviewForm({ onSubmitChangeTab }) {
             <option value="CPSC425">CPSC 425</option>
             <option value="CPSC427">CPSC 427</option>
             <option value="CPSC430">CPSC 430</option>
-            <option value="CPSC436a">CPSC 436a</option>
+            <option value="CPSC436a">CPSC 436a</option> */}
             <option value="CPSC436c">CPSC 436c</option>
-            <option value="CPSC436e">CPSC 436e</option>
+            {/* <option value="CPSC436e">CPSC 436e</option>
             <option value="CPSC436n">CPSC 436n</option>
             <option value="CPSC436r">CPSC 436r</option>
             <option value="CPSC436s">CPSC 436s</option>
@@ -165,8 +165,8 @@ function CoursesReviewForm({ onSubmitChangeTab }) {
             <option value="CPSC447">CPSC 447</option>
             <option value="CPSC448a">CPSC 448a</option>
             <option value="CPSC448b">CPSC 448b</option>
-            <option value="CPSC449">CPSC 449</option>
-            <option value="CPSC504">CPSC 504</option>
+            <option value="CPSC449">CPSC 449</option> */}
+            {/* <option value="CPSC504">CPSC 504</option>
             <option value="CPSC508">CPSC 508</option>
             <option value="CPSC509">CPSC 509</option>
             <option value="CPSC521">CPSC 521</option>
@@ -191,7 +191,7 @@ function CoursesReviewForm({ onSubmitChangeTab }) {
             <option value="CPSC545">CPSC 545</option>
             <option value="CPSC548">CPSC 548</option>
             <option value="CPSC550">CPSC 550</option>
-            <option value="CPSC554x">CPSC 554x</option>
+            <option value="CPSC554x">CPSC 554x</option> */}
           </Form.Select>
         </Form.Group>
         <Form.Group className="mb-3" controlId="year">
